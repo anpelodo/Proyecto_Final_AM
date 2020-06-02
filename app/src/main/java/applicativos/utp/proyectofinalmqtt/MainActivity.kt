f9ded2f100e892d0d148bedf41c5854c905ac314
anpelodo <53635026+anpelodo@users.noreply.github.com>
2020-06-02 12:48:59 -0500

    Initial commit

---
+++ app/src/main/java/applicativos/utp/proyectofinalmqtt/MainActivity.kt
@@ -0,0 +1,11 @@
+package applicativos.utp.proyectofinalmqtt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
