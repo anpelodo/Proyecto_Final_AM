c833d9849f36f78d7af4279a94445326c378c571
JheissonUrbano <66327221+JheissonUrbano@users.noreply.github.com>
2020-06-10 22:28:18 -0500

    variable

--- app/src/main/java/applicativos/utp/proyectofinalmqtt/MainActivity.kt
+++ app/src/main/java/applicativos/utp/proyectofinalmqtt/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "MQTT Message"
 
+    val Reg= "123"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
