9e872db1f9fa1f7235447f25a7ddad19612ee47a
JheissonUrbano <66327221+JheissonUrbano@users.noreply.github.com>
2020-06-11 16:50:50 -0500

    Inicio submenu,registro, tabbed

---
+++ app/src/main/java/applicativos/utp/proyectofinalmqtt/Registro.kt
@@ -0,0 +1,11 @@
+package applicativos.utp.proyectofinalmqtt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro2)
+    }
+}
