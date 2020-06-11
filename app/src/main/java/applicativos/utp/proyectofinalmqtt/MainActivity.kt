a14dd7968bc37e015ee5e4fe31559a997e358b0b
anpelodo <53635026+anpelodo@users.noreply.github.com>
2020-06-11 12:38:28 -0500

    Prueba de logs correcta. se configuran mejor las dependencias de MQTT, y se agrega el archivo JSON de Firebase

--- app/src/main/java/applicativos/utp/proyectofinalmqtt/MainActivity.kt
+++ app/src/main/java/applicativos/utp/proyectofinalmqtt/MainActivity.kt
@@ -2,19 +2,26 @@ package applicativos.utp.proyectofinalmqtt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "MQTT Message"
-
+    var buttontest: Button ?= null
     val Reg= "123"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttontest = findViewById(R.id.button)
     }
 
     override fun onResume() {
         super.onResume()
         //comentario
+
+        buttontest!!.setOnClickListener {
+            Log.i(TAG, "Prueba de los logs")
+        }
     }
 }
