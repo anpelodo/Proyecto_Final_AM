3d0307e49f9711d8feaf3ec8538d908a79b03fc3
anpelodo <53635026+anpelodo@users.noreply.github.com>
2020-06-16 12:57:36 -0500

    Configuración de nueva clave de API y conexión entre la otra pantalla

--- app/src/main/java/applicativos/utp/proyectofinalmqtt/ActivityDeviceList.kt
+++ app/src/main/java/applicativos/utp/proyectofinalmqtt/ActivityDeviceList.kt
@@ -1,7 +1,9 @@
 package applicativos.utp.proyectofinalmqtt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,6 +13,7 @@ class ActivityDeviceList : AppCompatActivity() {
     var vista: RecyclerView ?= null
 
     var auth = MainActivity.auth
+    val cont = this
 
     var info = ArrayList<deviceInfo>()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +27,17 @@ class ActivityDeviceList : AppCompatActivity() {
         vista = findViewById(R.id.contenedor)
         vista!!.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         val adapter = adaptador(info, object: ClickListener{
+            val i = Intent(cont, Tabbed::class.java)
+
             override fun onClick(vista: View, posicion: Int) {
                 Toast.makeText(applicationContext, info[posicion].id, Toast.LENGTH_SHORT).show()
+
+                try {
+                    startActivity(i)
+                } catch (e: Exception) {
+                    Toast.makeText(cont, "Algo salió mal", Toast.LENGTH_SHORT).show()
+                    Log.d("Open.Activity.Tabbed", "Error al abrir")
+                }
             }
         })
         vista!!.adapter = adapter
