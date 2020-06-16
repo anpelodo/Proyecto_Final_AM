7e556f058033e751b744f39ba6548e4d2462d6ba
JheissonUrbano <66327221+JheissonUrbano@users.noreply.github.com>
2020-06-15 22:19:33 -0500

    Parematros,Checkbox y boton

--- app/src/main/java/applicativos/utp/proyectofinalmqtt/MapsFragment.kt
+++ app/src/main/java/applicativos/utp/proyectofinalmqtt/MapsFragment.kt
@@ -1,12 +1,17 @@
 package applicativos.utp.proyectofinalmqtt
 
+import android.content.Context
 import android.graphics.Color
+import android.location.Location
+import android.location.LocationManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,6 +24,10 @@ import com.google.android.gms.maps.model.PolygonOptions
 
 class MapsFragment : Fragment() {
 
+    var googleMapFusedLocationProviderClient:FusedLocationProviderClient?=null
+    var mLastLocation: Location?=null
+    var mLocationRequest:LocationRequest?=null
+
     private val callback = OnMapReadyCallback { googleMap ->
 
         val colombia = LatLng(4.7936177,-75.69129)
@@ -26,19 +35,31 @@ class MapsFragment : Fragment() {
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(colombia))
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(colombia,15f))
 
+
     }
 
     override fun onCreateView(
+
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_maps, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+
+
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
 }
--- app/src/main/java/applicativos/utp/proyectofinalmqtt/pagina2.kt
+++ app/src/main/java/applicativos/utp/proyectofinalmqtt/pagina2.kt
@@ -6,8 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 
 class pagina2 : Fragment() {
+    internal lateinit var Boton1: CheckBox
+    var B1=0
+    internal lateinit var Boton2: CheckBox
+    var B2=0
+
+    var Time: EditText? = null
+    var Obtener: Button? = null
+    var Visor: TextView? = null
 
     companion object {
         fun newInstance() = pagina2()
@@ -20,6 +29,11 @@ class pagina2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var vista2= inflater.inflate(R.layout.pagina2_fragment, container, false)
+        Boton1=vista2.findViewById(R.id.checkBox1)
+        Boton2=vista2.findViewById(R.id.checkBox2)
+        Time=vista2.findViewById(R.id.dato)
+        Obtener=vista2.findViewById(R.id.enviar)
+        Visor=vista2.findViewById(R.id.visor)
 
         return vista2
     }
@@ -28,6 +42,27 @@ class pagina2 : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(Pagina2ViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Boton1!!.setOnClickListener {
+            if (Boton1.isChecked){ B1=1;
+                Toast.makeText(context,"Acepto1",Toast.LENGTH_SHORT).show()}
+            else{ B1=0;
+                Toast.makeText(context,"Nego1",Toast.LENGTH_SHORT).show()}
+        }
+        Boton2!!.setOnClickListener {
+            if (Boton2.isChecked){ B2=1;
+                Toast.makeText(context,"Acepto2",Toast.LENGTH_SHORT).show()}
+            else{ B2=0;
+                Toast.makeText(context,"Nego2",Toast.LENGTH_SHORT).show()}
+        }
+       Obtener!!.setOnClickListener {
+            Toast.makeText(context,"Obteniendo Datos",Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 }
